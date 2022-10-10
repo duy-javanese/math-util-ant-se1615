335c4f75a49cce7d339d8734d47a59e03cd9dfd1
phaiduykota <phaiduykota@gmail.com>
2022-10-10 16:36:55 +0700

    update getFactorial() to recursive method

--- src/com/duy/mathutil/core/MathUtil.java
+++ src/com/duy/mathutil/core/MathUtil.java
@@ -17,6 +17,35 @@ public class MathUtil {
     //vì giai thừa tăng cực nhanh, 21! tràn kiểu long (18 số 0)
     //ko có giai thừa âm
     //quy ước: n nhận vào từ 0..20!
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid argument. n must be between 0..20!");
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        long product = 1; //tích khởi đầu là 1, biến tích luỹ acc
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//        return product;
+//    }
+    //viết tính giai thừa theo đệ quy
+    //n! = 1.2.3.4...n
+    //c1: viết kiểu for, con heo đất, nhồi dần kết quả vào 1 biến
+    //product = product * i; //i chạy từ 1...n
+    //c2: đệ quy - recursion
+    //GỌI LẠI CHÍNH MÌNH VỚI 1 QUY MÔ/GIÁ TRỊ KHÁC
+    //Búp bê của người Nga: con búp bê bự chứa con búp bê nhỏ bên trong
+    //                      con trong chứa con khác... khoảng 12 con
+    //                      lặp lại chính mình 12 lần
+    //5! = ??? 1.2.3.4.5
+    //5! =          4!.5 = 5 x 4!
+    //4! = 4 x 3!
+    //3! = 3 x 2!
+    //2! = 2 x 1!
+    //1! = 1
+    //n! = n x (n - 1)!
     public static long getFactorial(int n) {
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid argument. n must be between 0..20!");
@@ -24,11 +53,13 @@ public class MathUtil {
         if (n == 0 || n == 1) {
             return 1;
         }
-        long product = 1; //tích khởi đầu là 1, biến tích luỹ acc
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+
+        //sống sót đến đây n = 2...20
+        //hàm này đang tính n! mà n! = n * (n - 1)!
+        
+        return n * getFactorial(n - 1);
+        
+        //n! = n * (n - 1)!
     }
 
     //tư duy viết code theo kiểu TDD - Test Driven Development
